0178cdc0948a9ffb047e5f6838244c5c73412ce5
Guadalupe <penaegeaguadalupe@gmail.com>
2026-02-25 12:18:52 +0100

    chore: setup base structure springboot

---
+++ src/main/java/com/swapstyle/swapstyle/SwapstyleApplication.java
@@ -0,0 +1,13 @@
+package com.swapstyle.swapstyle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwapstyleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwapstyleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swapstyle/swapstyle/SwapstyleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swapstyle.swapstyle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwapstyleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
