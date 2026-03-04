8d867c0a032faf2b321df1a560622f7934ea687c
Leonela88 <rivasaela@gmail.com>
2026-03-04 23:05:59 +0100

    chore: service methods

--- src/main/java/com/swapstyle/swapstyle/service/ArticleService.java
+++ src/main/java/com/swapstyle/swapstyle/service/ArticleService.java
@@ -1,5 +1,17 @@
 package com.swapstyle.swapstyle.service;
 
+import java.util.List;
+
+import com.swapstyle.swapstyle.entity.Article;
+
 public interface ArticleService {
+
+public Article createArticle(Article article, Integer idUser );
+
+public List <Article>getAllArticles();
+
+public Article deleArticle(Integer idArticle);
+
+public Article updateArticle(Article article);
 
 }
