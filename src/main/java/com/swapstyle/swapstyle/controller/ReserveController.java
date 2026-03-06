f8b1b6f830643bd62f3766142c386f882571957e
majoz-t <majozta@gmail.com>
2026-03-06 14:53:10 +0100

    feat: order lists newest to oldest, fix: change messages to english

--- src/main/java/com/swapstyle/swapstyle/controller/ReserveController.java
+++ src/main/java/com/swapstyle/swapstyle/controller/ReserveController.java
@@ -7,7 +7,7 @@ import com.swapstyle.swapstyle.dto.request.ReserveRequestDTO;
 import com.swapstyle.swapstyle.dto.response.ReserveResponseDTO;
 import com.swapstyle.swapstyle.service.ReserveService;
 
-import jakarta.validation.Valid;
+//import jakarta.validation.Valid;
 
 import java.util.List;
 
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("api/v1/reserves")
--- src/main/java/com/swapstyle/swapstyle/service/ArticleServiceImpl.java
+++ src/main/java/com/swapstyle/swapstyle/service/ArticleServiceImpl.java
@@ -1,5 +1,6 @@
 package com.swapstyle.swapstyle.service;
 
+import java.util.Comparator;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -7,60 +8,62 @@ import org.springframework.stereotype.Service;
 import com.swapstyle.swapstyle.dto.response.ArticleCardReponseDTO;
 import com.swapstyle.swapstyle.entity.Article;
 import com.swapstyle.swapstyle.repository.ArticleRepository;
-import java.time.LocalDateTime;
+//import java.time.LocalDateTime;
 import com.swapstyle.swapstyle.entity.enums.Category;
-import com.swapstyle.swapstyle.entity.enums.State;
-
+//import com.swapstyle.swapstyle.entity.enums.State;
 
 @Service
 public class ArticleServiceImpl implements ArticleService {
 
     private final ArticleRepository articleRepository;
 
-    //private final UserService userService;
+    // private final UserService userService;
 
-    public ArticleServiceImpl(ArticleRepository articleRepository, UserService userService){
+    public ArticleServiceImpl(ArticleRepository articleRepository, UserService userService) {
         this.articleRepository = articleRepository;
-       // this.userService= userService;
+        // this.userService= userService;
     }
 
     @Override
     public List<ArticleCardReponseDTO> getAllArticles() {
         List<Article> article = articleRepository.findAll();
+        if (article.isEmpty()) {
+            throw new RuntimeException("There's no articles published");
+        }
         return article.stream()
-        .map(a -> new ArticleCardReponseDTO(
-            a.getTitle(),
-            a.getSize(),
-            a.getPrice(),
-            a.getCategory(),
-            a.getState(),
-            a.getImage(),
-            a.getPublished(),
-            a.getUserOffers().getUserName()
-        ))
-        .toList();
+                .sorted(Comparator.comparing(Article::getPublished).reversed())
+                .map(a -> new ArticleCardReponseDTO(
+                        a.getTitle(),
+                        a.getSize(),
+                        a.getPrice(),
+                        a.getCategory(),
+                        a.getState(),
+                        a.getImage(),
+                        a.getPublished(),
+                        a.getUserOffers().getUserName()))
+                .toList();
     }
 
     @Override
     public List<ArticleCardReponseDTO> findByCategory(Category category) {
         List<Article> articles = articleRepository.findByCategory(category);
-        if (articles.isEmpty()){
-            throw new RuntimeException("No existen artículos para la categoría seleccionada");
+        if (articles.isEmpty()) {
+            throw new RuntimeException("Doesn't exist articles for the category selected");
         }
         return articles.stream()
-        .map(article -> new ArticleCardReponseDTO(
-            article.getTitle(),
-            article.getSize(),
-            article.getPrice(),
-            article.getCategory(),
-            article.getState(),
-            article.getImage(),
-            article.getPublished(),
-            article.getUserOffers().getUserName()
-        ))
-        .toList();
+                .sorted(Comparator.comparing(Article::getPublished).reversed())
+                .map(article -> new ArticleCardReponseDTO(
+                        article.getTitle(),
+                        article.getSize(),
+                        article.getPrice(),
+                        article.getCategory(),
+                        article.getState(),
+                        article.getImage(),
+                        article.getPublished(),
+                        article.getUserOffers().getUserName()))
+                .toList();
     }
-//COLLECTOR
-//OPTIONAL
+    // COLLECTOR
+    // OPTIONAL
 
- }
+}
